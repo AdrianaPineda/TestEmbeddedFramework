c8f9b4e2f21632e5beb4d0121cd6efc530ad2b25
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-02-29 16:13:32 -0500

    Update umbrella framework to fix build

---
+++ TestEmbeddedFramework/MainFile.swift
@@ -0,0 +1,14 @@
+//
+//  MainFile.swift
+//  TestEmbeddedFramework
+//
+//  Created by Adriana Pineda on 2/29/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+import Foundation
+import Coresdk
+
+class MainFile: NSObject {
+
+}
