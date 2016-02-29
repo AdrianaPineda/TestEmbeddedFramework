2da078fd648cbfa9404fbd7ab502e2908fa104f7
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-02-29 14:15:40 -0500

    Create project in xcode

---
+++ TestEmbeddedFramework/TestEmbeddedFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestEmbeddedFramework.h
+//  TestEmbeddedFramework
+//
+//  Created by Adriana Pineda on 2/29/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestEmbeddedFramework.
+FOUNDATION_EXPORT double TestEmbeddedFrameworkVersionNumber;
+
+//! Project version string for TestEmbeddedFramework.
+FOUNDATION_EXPORT const unsigned char TestEmbeddedFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestEmbeddedFramework/PublicHeader.h>
+
+
---
+++ TestEmbeddedFrameworkTests/TestEmbeddedFrameworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestEmbeddedFrameworkTests.swift
+//  TestEmbeddedFrameworkTests
+//
+//  Created by Adriana Pineda on 2/29/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+import XCTest
+@testable import TestEmbeddedFramework
+
+class TestEmbeddedFrameworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
